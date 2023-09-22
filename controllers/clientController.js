513a44286672c29b4269c1aa0c8d40faca506890
AliNayab Nathani <alinayab@inspedium.com>
2023-09-22 19:33:39 +0500

    nn

--- controllers/clientController.js
+++ controllers/clientController.js
@@ -1,6 +1,8 @@
 const User = require("../models/User");
 const Role = require("../models/Role");
 const ContentCreator = require("../models/ContentCreator");
+const Trailer = require("../models/Trailer");
+const Episode = require("../models/Episodes");
 const Channel = require("../models/Channel");
 const json2csv = require("json2csv");
 const fs = require("fs").promises;
@@ -9,73 +11,71 @@ const CustomError = require("../errors");
 const { STATUS_CODES } = require("http");
 const Video = require("../models/Video");
 const ContentApproval = require("../models/ContentApproval");
+const { Op } = require("sequelize");
+const sequelize = require("../config/sequelize");
 
 const MyVideos = async (req, res) => {
-  const videos = await Video.findAll({
-    raw: true,
+  const userId = "1";
+
+  const myShows = await Video.findAll({
+    where: {
+      channelId: {
+        [Op.in]: sequelize.literal(
+          `(SELECT id FROM channel WHERE content_creator_id = ${userId})`
+        ),
+      },
+    },
     attributes: [
       "id",
       "name",
       "views",
       "rented_amount",
       "purchasing_amount",
+      "Type",
       "createdAt",
       "channelId",
     ],
+    include: [{ model: Trailer, as: "trailers" }],
   });
 
-  if (!videos || videos.length === 0) {
-    return res
-      .status(StatusCodes.NOT_FOUND)
-      .json({ error: `No Videos found!!` });
+  if (!myShows || myShows.length === 0) {
+    throw new CustomError.NotFoundError(`No Videos found!!`);
   }
 
-  const videoIds = videos.map((video) => video.id);
+  const showCount = myShows.length;
+  res.status(StatusCodes.OK).json({ myShows, showCount });
+};
 
-  const contentApprovals = await ContentApproval.findAll({
-    where: { video_id: videoIds },
-    raw: true,
+const getSingleMyVideo = async (req, res) => {
+  const userId = "1";
+
+  const myShows = await Video.findAll({
+    where: {
+      channelId: {
+        [Op.in]: sequelize.literal(
+          `(SELECT id FROM channel WHERE content_creator_id = ${userId})`
+        ),
+      },
+    },
+    attributes: [
+      "id",
+      "name",
+      "views",
+      "rented_amount",
+      "purchasing_amount",
+      "Type",
+      "createdAt",
+      "channelId",
+    ],
+    include: [{ model: Trailer, as: "trailers" }],
   });
 
-  const videoStatusMap = {};
-  contentApprovals.forEach((approval) => {
-    videoStatusMap[approval.video_id] = approval.status;
-  });
+  if (!myShows || myShows.length === 0) {
+    throw new CustomError.NotFoundError(`No Videos found!!`);
+  }
 
-  videos.forEach((video) => {
-    video.status = videoStatusMap[video.id] || "Pending";
-  });
-
-  const channelIds = videos.map((video) => video.channelId);
-
-  const channels = await Channel.findAll({
-    where: { id: channelIds },
-    raw: true,
-    attributes: ["id", "content_creator_id"],
-  });
-
-  const creatorIds = channels.map((channel) => channel.content_creator_id);
-
-  const contentCreators = await ContentCreator.findAll({
-    where: { id: creatorIds },
-    raw: true,
-    attributes: ["id", "name"],
-  });
-
-  const creatorMap = contentCreators.reduce((map, creator) => {
-    map[creator.id] = creator.name;
-    return map;
-  }, {});
-
-  videos.forEach((video) => {
-    const channel = channels.find((channel) => channel.id === video.channelId);
-    if (channel) {
-      video.creator_name = creatorMap[channel.content_creator_id];
-    }
-  });
-
-  const videosCount = videos.length;
-  res.status(StatusCodes.OK).json({ videos, videosCount });
+  const showCount = myShows.length;
+  res.status(StatusCodes.OK).json({ myShows, showCount });
 };
 
 const getMyChannels = async (req, res) => {
