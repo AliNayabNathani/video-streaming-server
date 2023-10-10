16fed9cf648d6625a9cf5ee90ca1b7171020c3df
AliNayab Nathani <alinayab@inspedium.com>
2023-10-10 14:26:28 +0500

    nn

--- config/sequelize.js
+++ config/sequelize.js
@@ -2,15 +2,15 @@ const { Sequelize } = require("sequelize");
 
 const sequelize = new Sequelize({
   dialect: "postgres",
-  host: "dpg-ckhp3a6afg7c73frctsg-a",
+  host: "dpg-ckhp3a6afg7c73frctsg-a.oregon-postgres.render.com",
   port: 5432,
-  username: "videeodb_user",
+  username: "videodb_jkqb_user",
   password: "x79U393nDALkG7WcjlF4OvpVlea3usW9",
-  database: "videeodb",
+  database: "videodb_jkqb",
   dialectOptions: {
     ssl: {
-      require: true, // Set to true to require SSL
-      rejectUnauthorized: false, // Set to false to avoid rejection of self-signed certificates
+      require: true,
+      rejectUnauthorized: false,
     },
   },
 });
--- db/connect.js
+++ db/connect.js
@@ -1,13 +1,12 @@
 const { Pool } = require("pg");
 
 const dbConfig = {
-  user: "videodb_jkqb",
-  host: "dpg-ckhp3a6afg7c73frctsg-a",
-  database: "videeodb",
+  user: "videodb_jkqb_user",
+  host: "dpg-ckhp3a6afg7c73frctsg-a.oregon-postgres.render.com",
+  database: "videodb_jkqb",
   password: "x79U393nDALkG7WcjlF4OvpVlea3usW9",
   port: 5432,
 };
-
 const connectDB = () => {
   const pool = new Pool(dbConfig);
 
