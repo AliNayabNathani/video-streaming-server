92abc9cd9378f0b354923d9d21bf5651eb8f7dd0
AliNayab Nathani <alinayab@inspedium.com>
2023-10-10 14:13:16 +0500

    nn

--- config/sequelize.js
+++ config/sequelize.js
@@ -2,11 +2,17 @@ const { Sequelize } = require("sequelize");
 
 const sequelize = new Sequelize({
   dialect: "postgres",
-  host: "localhost",
+  host: "dpg-ckhp3a6afg7c73frctsg-a",
   port: 5432,
-  username: "postgres",
-  password: "inspedium",
-  database: "videoserver",
+  username: "videeodb_user",
+  password: "x79U393nDALkG7WcjlF4OvpVlea3usW9",
+  database: "videeodb",
+  dialectOptions: {
+    ssl: {
+      require: true, // Set to true to require SSL
+      rejectUnauthorized: false, // Set to false to avoid rejection of self-signed certificates
+    },
+  },
 });
 
 module.exports = sequelize;
