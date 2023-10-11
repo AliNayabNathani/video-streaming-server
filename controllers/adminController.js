26c025d2a1f2b5703117cb29df015cbba642e4fe
AliNayab Nathani <alinayab@inspedium.com>
2023-10-11 11:11:58 +0500

    nn

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -249,7 +249,7 @@ const getAllContentCreator = async (req, res) => {
         // attributes: ["gender"],
       });
 
-      const genderValue = user ? user.gender : "-";
+      const genderValue = user && user.gender ? user.gender : "Not Specified";
 
       return {
         id: creator.id,
